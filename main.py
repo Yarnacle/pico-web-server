44c751b42f1cd72a922a977034da266a1c00e9b6
Yarnacle <109094587+Yarnacle@users.noreply.github.com>
2023-02-25 22:36:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from led_control.led_control import led_control
 led.on()
 
 server = webserver.Webserver()
-server.connect('RoseWoods-2G','ywcL5337,8')
+server.connect('WIFI','PASSWORD')
 server.path('/led',handler = led_control)
 
 server.listen()
